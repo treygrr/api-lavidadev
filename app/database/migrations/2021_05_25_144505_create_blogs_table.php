24487a409683c0775773bcbf59487115915a31b7
treygrr <phlasc@gmail.com>
2021-05-25 11:46:50 -0400

    Add blog migration factory seeder model

---
+++ app/database/migrations/2021_05_25_144505_create_blogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->text('content');
+            $table->foreign('author')->references('id')->on('users');
+            $table->foreign('cover_photo')->references('id')->on('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
