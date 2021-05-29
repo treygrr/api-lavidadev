b731d20201f23f9eff61f8d4b1bb0b83d6eccb60
treygrr <phlasc@gmail.com>
2021-05-28 21:58:33 -0400

    Add image model, factory, seeder, and migration

--- app/app/Models/image.php
+++ app/app/Models/image.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class image extends Model
+class Image extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'filename',
+        'extension',
+        'blog_id',
+        'alt',
+    ];
 }
